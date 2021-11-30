a57ab9f621cd96d446c0dae60dd1d99cc298959d
Ola Hola <homoncik.aleksandra@gmail.com>
2021-11-30 17:35:22 +0100

    adds random Meme photo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Meme from './Components/Meme/Meme';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
-}
+} 
---
+++ src/Components/Meme/Meme.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Meme.css"
+import memesData from "../memesData";
+
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData  )
+    
+    function getMemeImage() {
+        const myArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random()* myArray.length)
+        const url = myArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme, 
+            randomImage: url
+        }))
+    }
+    return(
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button
+                    className="form--button"
+                    onClick={ getMemeImage }
+                >
+                    Get a new meme image 🖼</button>
+            </div>
+            <img src={ meme.randomImage } className="meme--image" />
+        </main>
+    )
+}
