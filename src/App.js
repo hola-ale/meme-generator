989a09e9f8f6e8c1ba70f57a0d5bdace6d08e533
Ola Hola <homoncik.aleksandra@gmail.com>
2021-11-30 13:24:09 +0100

    adds a header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
     </div>
   );
 }
-
-export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css"
+
+export default function Header() {
+    return(
+        <header className="header">
+            <h2>Meme Generator</h2>
+            <h4>React course project</h4>
+        </header>
+    )
+}
